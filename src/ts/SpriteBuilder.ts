05c228bb48ebaaf4fbf9da7e017f1aaa7ca88173
noonworks <noonworks@gmail.com>
2019-06-07 18:45:28 +0900

    Add sprite H.

--- src/ts/SpriteBuilder.ts
+++ src/ts/SpriteBuilder.ts
@@ -6,7 +6,7 @@ const TO_RADIAN = Math.PI / 180;
 const BASE_ALPHA = 0.82;
 
 export function addSprites(theater: ITheater, img: IHatsu): void {
-  [HatsuA, HatsuB, HatsuD, HatsuC, HatsuE, HatsuF, HatsuG].forEach((h) => {
+  [HatsuA, HatsuB, HatsuD, HatsuC, HatsuE, HatsuF, HatsuG, HatsuH].forEach((h) => {
     theater.addHatsu(new HatsuSprite({ img, draw: h }));
   });
 }
@@ -225,4 +225,45 @@ export function HatsuG(theater: ITheater, img: IHatsu) {
   theater.context.restore();
 }
 
-const WHOLE_MSEC = G_END + 1500;
+const H_START = 23770;
+const H_END = 28920;
+const H_LENGTH = H_END - H_START;
+const H_R_HORIZON = 27720;
+const H_ZOOM = 1.2;
+export function HatsuH(theater: ITheater, img: IHatsu) {
+  const msec = modular(theater.msec) - H_START;
+  if (msec < 0 || msec > H_LENGTH) { return; }
+  const { dw, dh } = hatsuSize(theater, img);
+  // ※方程式は發の中心点の軌跡
+  // 始点S
+  const Sx = dh * H_ZOOM / 2;
+  const Sy = dh * H_ZOOM / 2 * -1;
+  // 点P
+  const Px = theater.width16;
+  const Py = theater.height * 7 / 9;
+  // 2点Qを通る y = ax + b
+  // b = -ax + y
+  const a = (Py - Sy) / (Px - Sx);
+  const b = -1 * a * Px + Py;
+  // 終点E
+  const Ex = theater.width16 + dh * H_ZOOM / 2;
+  const Ey = a * Ex + b;
+  // 座標
+  const x = liner(Sx, Ex, H_LENGTH, msec);
+  if (x > Ex || x < Sx) { return; }
+  const y = liner(Sy, Ey, H_LENGTH, msec);
+  if (y > Ey || y < Sy) { return; }
+  // 回転率の方程式 r = c * msec
+  const c = 270 / (H_R_HORIZON - H_START);
+  const r = c * msec;
+  theater.context.save();
+  theater.context.translate(x, y);
+  theater.context.rotate(r * TO_RADIAN);
+  theater.context.globalAlpha = BASE_ALPHA;
+  theater.context.drawImage(img.img, 0, 0, img.width, img.height,
+    dw * H_ZOOM * -0.5, dh * H_ZOOM * -0.5, dw * H_ZOOM, dh * H_ZOOM);
+  theater.context.globalAlpha = 1.0;
+  theater.context.restore();
+}
+
+const WHOLE_MSEC = H_END + 1500;
