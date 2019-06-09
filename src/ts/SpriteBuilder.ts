ec692344ba6458203a91023e0256f0e00eabffef
noonworks <noonworks@gmail.com>
2019-06-09 16:08:07 +0900

    Add sprite I.

--- src/ts/SpriteBuilder.ts
+++ src/ts/SpriteBuilder.ts
@@ -6,7 +6,7 @@ const TO_RADIAN = Math.PI / 180;
 const BASE_ALPHA = 0.82;
 
 export function addSprites(theater: ITheater, img: IHatsu): void {
-  [HatsuA, HatsuB, HatsuD, HatsuC, HatsuE, HatsuF, HatsuG, HatsuH].forEach((h) => {
+  [HatsuA, HatsuB, HatsuD, HatsuC, HatsuE, HatsuF, HatsuG, HatsuH, HatsuI].forEach((h) => {
     theater.addHatsu(new HatsuSprite({ img, draw: h }));
   });
 }
@@ -23,6 +23,15 @@ function hatsuSize(theater: ITheater, img: IHatsu): { dw: number, dh: number } {
 
 function liner(vStart: number, vEnd: number, length: number, msec: number): number {
   return (vEnd - vStart) * msec / length + vStart;
+}
+
+function easeInOutCubicPercent(msec: number, length: number) {
+  const t = msec / (length / 2);
+  if (t < 1) {
+    return 1.0 / 2.0 * t * t * t;
+  }
+  const t2 = t - 2;
+  return 1.0 / 2.0 * (t2 * t2 * t2 + 2);
 }
 
 const A_START = 660;
@@ -266,4 +275,37 @@ export function HatsuH(theater: ITheater, img: IHatsu) {
   theater.context.restore();
 }
 
-const WHOLE_MSEC = H_END + 1500;
+const I_START = 27380;
+const I_END = 33100;
+const I_LENGTH = I_END - I_START;
+const I_PATH = [14, 35.0, 0, 0, 13.2, -14.7, 28.8, -9.8, 16.7, 5.2, 20.2, 45.4, 21.7, 119.0];
+const I_VIEWBOX_W = 127;
+const I_PATHES: { [key: number]: SVGPathElement } = {};
+const I_ZOOM_MAX = 4.48;
+const I_ZOOM_MIN = 0.05;
+export function HatsuI(theater: ITheater, img: IHatsu) {
+  const msec = modular(theater.msec) - I_START;
+  if (msec < 0 || msec > I_LENGTH) { return; }
+  const { dw, dh } = hatsuSize(theater, img);
+  // 軌道パスから座標を取得
+  if (!I_PATHES.hasOwnProperty(theater.width16)) {
+    const np = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    const pathes = I_PATH.map((v, idx) => {
+      return '' + (v * theater.width16 / I_VIEWBOX_W) + ((idx % 2 === 0) ? ',' : ' ') + ((idx === 1) ? 'c ' : '');
+    });
+    np.setAttribute('d', 'm ' + pathes.join(''));
+    I_PATHES[theater.width16] = np;
+  }
+  const p = I_PATHES[theater.width16];
+  const pathLength = p.getTotalLength();
+  const point: DOMPoint = p.getPointAtLength(pathLength - msec * pathLength / I_LENGTH);
+  // 拡大率
+  const ep = easeInOutCubicPercent(msec, I_LENGTH);
+  const z = I_ZOOM_MAX - (I_ZOOM_MAX - I_ZOOM_MIN) * ep;
+  theater.context.globalAlpha = BASE_ALPHA;
+  theater.context.drawImage(img.img, 0, 0, img.width, img.height,
+    point.x - (dw * z / 2), point.y - (dh * z / 2), dw * z, dh * z);
+  theater.context.globalAlpha = 1.0;
+}
+
+const WHOLE_MSEC = I_END + 1500;
